6f00bdb4213e7b96ffa8014b1f9903718b3f8e0e
Leslie <leslie.reyes@vtex.com.br>
2022-08-12 13:45:49 +0200

    source width and styles  testing

--- src/components/TMehdi/TMehdi.tsx
+++ src/components/TMehdi/TMehdi.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable no-console */
 import useSWR from 'swr'
-// import { Image } from 'src/components/ui/Image'
+import { Banner, BannerContent, BannerImage, BannerLink } from '@faststore/ui'
 import { useSession } from '@faststore/sdk'
 
 type TMehdiVariant = {
@@ -14,6 +13,7 @@ export type TMehdiProps = TMehdiVariant
 
 const TMehdi = ({ title }: TMehdiProps) => {
   const { person } = useSession()
+
   let urlDesktop
   let urlMobile
   let hrefImg
@@ -33,28 +33,79 @@ const TMehdi = ({ title }: TMehdiProps) => {
   if (data) {
     urlDesktop = data.url
     urlMobile = data.urlMobile
-    hrefImg = data.href
+    hrefImg = data.hrefImg
   }
 
   return (
-    <div className="container">
-      <h2>{title}</h2>
+    <div
+      className="container"
+      style={{ margin: 'auto', paddingBottom: '32px' }}
+    >
       <div>
         {data && (
           <div>
-            <picture>
-              <source media="(min-width: 601px)" srcSet={urlDesktop} />
-              <source media="(max-width: 600px)" srcSet={urlMobile} />
-              <img
-                src="https://cdn.pixabay.com/photo/2022/07/21/06/56/ocean-7335499_1280.jpg"
-                alt="test"
-              />
-            </picture>
+            <section className="section">
+              <article
+                data-store-hero="true"
+                data-testid="store-hero"
+                data-fs-hero="true"
+                data-fs-hero-variant="primary"
+                data-fs-hero-color-variant="main"
+              >
+                <div
+                  data-hero-image="true"
+                  data-testid="store-hero-image"
+                  data-fs-hero-image="true"
+                >
+                  <a href={hrefImg}>
+                    <picture
+                      data-fs-image="true"
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <source
+                        media="(min-width: 601px)"
+                        srcSet={urlDesktop}
+                        width="300px"
+                        height="400px"
+                      />
 
-            <h1>{urlDesktop}</h1>
-            <h1>{urlMobile}</h1>
-            <h1>{hrefImg}</h1>
-            <p>{person?.id}</p>
+                      <source
+                        media="(max-width: 600px)"
+                        srcSet={urlMobile}
+                        width="500px"
+                        height="250px"
+                      />
+
+                      <img
+                        src="https://cdn.pixabay.com/photo/2022/07/21/06/56/ocean-7335499_1280.jpg"
+                        alt="test"
+                        width="360w"
+                      />
+                    </picture>
+                  </a>
+                </div>
+                <header
+                  data-hero-heading="true"
+                  data-testid="store-hero-heading"
+                  data-fs-hero-heading="true"
+                  aria-labelledby="hero-heading"
+                >
+                  <div data-fs-hero-wrapper="true" className="layout__content">
+                    <div data-fs-hero-info="true">
+                      <h1 id="hero-heading">{title}</h1>
+                      <p data-fs-hero-text-body="true">
+                        At BaseStore you can shop the best tech of 2022. Enjoy
+                        and get 10% off on your first purchase.
+                      </p>
+                    </div>
+                  </div>
+                </header>
+              </article>
+            </section>
           </div>
         )}
         {error && <h1>{error}</h1>}
